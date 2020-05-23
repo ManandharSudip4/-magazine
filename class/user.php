f6012e54d1c425e5c186f2b1c87e79b00f0ce175
ManandharSudip4 <manandharsudip8@gmail.com>
2020-05-23 11:23:28 +0545

    Database Module for Project

--- class/user.php
+++ class/user.php
@@ -1,10 +1,68 @@
 <?php
 	class user extends database{
 		function __construct(){
-			%\$this->table = 'users';
+			$this->table = 'users';
 			database::__construct();
 		}
-		public function addUSer($data)
+		// public function addUser($data,$is_die=false){
+		// 	return $this->addData($data,$is_die);
+		// }
+		public function getUserbyId($user_id,$is_die=false){
+			
+			$args = array(
+				'fields' => array('id','username','email','password'),
+				'where'	=> array(
+					'and' => array(
+							'id' => $user_id,
+						)
+					)
+				// ,
+				// 'order' => 'ASC',
+				// 'limit' => array(
+				// 		'offset' => 6,
+				// 		'no_of_data'=>7
+				//	)
+				);
+
+			return $this->getData($args,$is_die);
+		}
+		public function getUserbyEmail($email,$is_die=false){
+			
+			$args = array(
+				'fields' => array('id','username','email','password'),
+				'where'	=> array(
+					'and' => array(
+							'email' => $email,
+						)
+					)
+				);
+
+			return $this->getData($args,$is_die);
+		}
+
+		public function updateUserbyEmail($data,$email,$is_die=false){
+			$args = array(
+				'where'	=> array(
+					'and' => array(
+							'email' => $email,
+						)
+					)
+				);
+
+			return $this->updateData($data,$args,$is_die);
+		}
+
+		public function deleteUserbyEmail($email,$is_die=false){
+			$args = array(
+				'where'	=> array(
+					'and' => array(
+							'email' => $email,
+						)
+					)
+				);
+
+			return $this->deleteData($args,$is_die);
+		}
 	}
 
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 	include $_SERVER['DOCUMENT_ROOT'].'config/init.php';
-	redirect('cms/index');  
+	redirect('cms/index'); 
+
+	$user = new user();
+	$data = array(
+		'username'=>'ms',
+		'email'=>'manandharsudip8@gmail.com',
+		'role'=>'Admin',
+		'session_token'=>tokenize()
+	); 
+	#$user->addUser($data);
+	#$datas=$user->getUserbyEmail('manandharsudip8@gmai.com');
+	// $datas=$user->updateUserbyEmail($data,'manandharsudip8@gmai.com');
+	// debugger($datas);
+	$user->deleteUserbyEmail('manandharsudip8@gmail.com');
 ?>
